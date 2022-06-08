5b1d2446d61eb44a6bfc2b8eb189d4e4c9ae46d3
María José Membreño Herrera <Maria Membreno>
2022-06-08 17:01:35 -0600

    upload plugin for car rental

---
+++ car-rental.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Car Rental Plugin
+ * Plugin URI: http://www.example.com/
+ * Description: This plugin allow create car rental reserves, car managements, send car reserves, season managements, prices, etc .
+ * Version: 1.0.0
+ * Author: Maria Membreño
+ * Author URI: http://www.example.com/
+ * License: GPLv2 or later
+ * Text Domain: car-rental
+ */
+
+ 
